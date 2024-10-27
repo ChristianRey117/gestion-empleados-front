d730af39e31496f993cff9f2fa47c25a460672db
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-10-27 14:45:37 -0400

    feat(employee-component): use list-employeee component in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListEmployeeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gestion-empleados-front';
