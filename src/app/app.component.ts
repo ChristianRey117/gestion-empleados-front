e349bd8901980cf09970cbee330a66523b516655
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-10-28 01:07:29 -0400

    feat(routes): use route empleados as default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListEmployeeComponent],
+  imports: [
+    RouterOutlet,
+    ListEmployeeComponent,
+    HttpClientModule,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'empleados',
+    component: ListEmployeeComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'empleados',
+    pathMatch: 'full',
+  },
+];
