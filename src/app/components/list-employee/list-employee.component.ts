ab31524289c56bde20f4c9952fa1c8428d7cb911
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-10-27 23:07:44 -0400

    feat(list-employee): loop and show data of all employees

--- src/app/components/list-employee/list-employee.component.ts
+++ src/app/components/list-employee/list-employee.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IEmployee } from '../../interfaces/IEmployee';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-employee',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-employee.component.html',
-  styleUrl: './list-employee.component.scss'
+  styleUrl: './list-employee.component.scss',
 })
-export class ListEmployeeComponent {
+export class ListEmployeeComponent implements OnInit {
+  listOfEmployee: IEmployee[];
 
+  ngOnInit(): void {
+    this.listOfEmployee = [
+      {
+        id: 1,
+        name: 'Christian',
+        lastName: 'Reyes',
+        email: 'christian@gmail.com',
+      },
+    ];
+  }
 }
